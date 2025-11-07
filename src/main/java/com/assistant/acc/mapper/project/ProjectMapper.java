63bea687eb1f7891e43b0e612a837e871918b65c
rmfhrm <rmfhrm9@gmail.com>
2025-11-07 16:10:57 +0900

    project과련 파일 수정

--- src/main/java/com/assistant/acc/mapper/project/ProjectMapper.java
+++ src/main/java/com/assistant/acc/mapper/project/ProjectMapper.java
@@ -1,5 +1,7 @@
 package com.assistant.acc.mapper.project;
 
+import com.assistant.acc.domain.project.Project;
+import com.assistant.acc.domain.project.UserInput;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
@@ -7,5 +9,12 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface ProjectMapper {
+
+    // 1. 새 프로젝트 생성 (p_no가 DTO로 반환됨)
+    void insertProject(Project project);
+    // 2. 사용자의 초기 입력을 저장
+    void insertInitialUserInput(UserInput userInput);
+    // 3. AI 분석 완료 후, JSON 결과들을 업데이트
+    void updateAnalysissResults(UserInput userInput);
     
 }
