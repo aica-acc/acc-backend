120b2a7aa6d2281b7a6672efdb2bd9fe890c1bf9
rmfhrm <rmfhrm9@gmail.com>
2025-12-04 10:41:53 +0900

    report imgae file

--- src/main/java/com/assistant/acc/mapper/editor/PromotionPathMapper.java
+++ src/main/java/com/assistant/acc/mapper/editor/PromotionPathMapper.java
@@ -3,6 +3,8 @@ package com.assistant.acc.mapper.editor;
 import com.assistant.acc.dto.editor.PromotionPathDTO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface PromotionPathMapper {
 
@@ -10,4 +12,5 @@ public interface PromotionPathMapper {
 
     // 필요하면 나중에 이런 것도 추가 가능:
     // List<PromotionPathDTO> findByPNo(@Param("pNo") int pNo);
+    List<PromotionPathDTO> findAllByPNo(Integer pNo);
 }
